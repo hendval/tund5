ad0959dd92a0ef906a240237df5a2e86d26b88c7
Hendrik Vallimägi <hendval@greeny.cs.tlu.ee>
2015-09-30 11:45:51 +0300

    loginyo

--- data.php
+++ data.php
@@ -1,3 +1,14 @@
 <?php
+	require_once("functions.php");
 
+	// kui kasutaja ei ole sisse logitud
+	// siis suunan data.php lehele
+	if(!isset($_SESSION["logged_in_user_id"])){
+		header("Location: login.php");
+	}
+	
 ?>
+<p>
+	Tere, <?=$_SESSION["logged_in_user_email"];?>
+	<br><a href="?logout=1">Logi välja</a>
+</p>
